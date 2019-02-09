6f3132fc8085fce40a8367493ea637b0239c8947
alcantara-erick <42950578+alcantara-erick@users.noreply.github.com>
2019-02-09 15:22:29 -0500

    Initial commit

---
+++ ArcadiaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArcadiaViewController.swift
+//  ISpy
+//
+//  Created by Erick Alcantara on 2/5/19.
+//  Copyright © 2019 Erick Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class ArcadiaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StateViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Erick Alcantara on 2/5/19.
+//  Copyright © 2019 Erick Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class StateViewController: UIViewController {
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
